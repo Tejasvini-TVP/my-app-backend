acc3c162b38e8a9e7bf641eee5bb68508dca3da7
Tejasvin-TVP <Tejasvini.patel1311@gmail.com>
2024-04-24 18:23:34 +0530

    post

--- src/server.js
+++ src/server.js
@@ -5,14 +5,17 @@ import express from 'express';
 let articlesInfo = [{
     name: 'learn-react',
     upvotes: 0,
+    Comments: [],
 },
  {
    name: 'learn-node',
-   upvote: 0 
+   upvote: 0,
+   Comments: [],
  },
 {
     name: 'mongodb',
     upvote: 0,
+    Comments: [],
 }]
 const app = express();
 app.use(express.json());
@@ -38,6 +41,10 @@ app.put('/api/articles/:name/upvote', (req,res) => {
    } else {
     res.send('That article doesn\'t exist');
    }
+})
+
+app.post('/api/articles/:name/comments',(req,res) => {
+    
 })
 app.listen(8000, () => {
     console.log('Server is listening on port 8000');
