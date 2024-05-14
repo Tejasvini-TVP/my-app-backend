b8d822abbc6dd7200ac5f5c2434b0c412db26084
Tejasvin-TVP <Tejasvini.patel1311@gmail.com>
2024-05-14 18:20:24 +0530

    touch up

--- src/server.js
+++ src/server.js
@@ -1,17 +1,66 @@
+// import fs from 'fs';
+import path from 'path';
+import admin from 'firebase-admin';
 import express from "express";
 import { db,connectToDb } from './db.js'
 
+
+import { fileURLToPath } from 'url';
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.__dirname(__filename);
+// const credentials = JSON.parse(
+//   fs.readFileSync('./credentials.json')
+// )
+
+// admin.initializeApp({
+//   credential:admin.credential.cert(credentials),
+// })
 const app = express();
 app.use(express.json());
+app.use(express.static(path.join(__dirname,'../build')));
+
+app.get(/^(?!\/api).+/,(req,res) => {
+  res.sendFile(path.join(__dirname,'../build/index.html'));
+})
+
+app.use(async (req,res, next) => {
+  const  {authtoken } =  req.headers;
+
+
+  if (authtoken){
+    try{
+      req.user = await admin.auth().verifyIdToken(authtoken);
+    } catch (e) {
+      res.sendStatus(400);
+    }
+  }
+  req.user = req.user || {};
+  next();
+})
 
 app.get("/api/articles/:name", async (req, res) => {
   const { name } = req.params;
-
+ const {uid} = req.user;
  
   const article = await db.collection("articles").findOne({ name });
 
-  res.json(article);
+  if (article){
+    const upvoteIds = article.upvoteIds || [];
+    article.canUpvote = uid && !upvoteIds.include(uid);
+    res.json(article);
+  }else{
+    res.sendStatus(404) ;
+  }
+
 });
+
+ app.use((req,res,next) => {
+  if (req.user) {
+    next();
+  }else {
+    res.sendStatus(401);
+  }
+ })
 // app.post('/hello', (req, res) => {
 //    res.send(`Hello ${req.body.name}!`);
 // });
@@ -24,18 +73,24 @@ app.get("/api/articles/:name", async (req, res) => {
 
 app.put("/api/articles/:name/upvote", async(req, res) => {
   const { name } = req.params;
- 
-  
-   await db.collection('articles').updateOne({ name}, {
-    $inc: { upvotes: 1},
-    // $set: { upvotes: 100}
-   })
-
-const article = await db.collection('articles').findOne({ name});
+ const {uid} = req.user;
+  const article = await db.collection("articles").findOne({ name });
 
   if (article) {
-    article.upvotes += 1;
-    res.json(article);
+    const upvoteIds = article.upvoteIds || [];
+    article.canUpvote = uid && !upvoteIds.include(uid);
+
+    if(canUpvote) {
+        await db.collection('articles').updateOne({ name}, {
+        $inc: { upvotes: 1},
+        $push: {upvoteIds: uid},
+       })
+    }
+
+   
+
+const updatedArticle = await db.collection('articles').findOne({ name});
+   res.json(updatedArticle);
   } else {
     res.send("That article doesn't exist");
   }
@@ -43,11 +98,11 @@ const article = await db.collection('articles').findOne({ name});
 
 app.post("/api/articles/:name/comments", async(req, res) => {
   const { name } = req.params;
-  const { postedBy, text } = req.body;
-
+  const { text } = req.body;
+  const { email} = req.user;
   
   await db.collection('articles').updateOne({ name},{
-    $push: {comments: {postedBy,text} },
+    $push: {comments: {postedBy: email,text} },
   })
 
   const article = await db.collection('articles').findOne({ name });
@@ -59,10 +114,11 @@ app.post("/api/articles/:name/comments", async(req, res) => {
   }
 });
 
+const PORT = process.env.PORT || 8000;
 
 connectToDb(() => {
     console.log('Successfully connected to database!')
-    app.listen(8000, () => {
-    console.log("Server is listening on port 8000");
+    app.listen(PORT, () => {
+    console.log("Server is listening on port" + PORT);
     })
 })
